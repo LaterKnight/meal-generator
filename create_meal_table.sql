6b1adc2fe3af67f3a00dd05cfef8173f5741b6ff
LaterKnight <email.ben.knight@googlemail.com>
2025-02-22 22:39:03 +0000

    Updating create_meal_table.sql with comments to improve understanding

--- create_meal_table.sql
+++ create_meal_table.sql
@@ -1,4 +1,4 @@
--- Creates the main "meals" table.
+-- Creates the main "meals" table. You will need to run this first to set up the table(s) before you can run the program.
 CREATE TABLE meals(
     meal_id INTEGER PRIMARY KEY,
     meal_name TEXT NOT NULL UNIQUE,
